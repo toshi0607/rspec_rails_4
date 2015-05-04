c0a510891612e540edb9ffe6d9f90a13335c7493
Tshinori Sugita <sugita@c-fo.com>
2015-05-04 14:35:39 +0900

    initial settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,37 +1,42 @@
-require 'rubygems'
-require 'spork'
-require 'database_cleaner'
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
 
-Spork.prefork do
-  ENV["RAILS_ENV"] ||= 'test'
-  require File.expand_path("../../config/environment", __FILE__)
-  require 'rspec/rails'
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
-  RSpec.configure do |config|
-    config.use_transactional_fixtures = false
-    config.infer_base_class_for_anonymous_controllers = false
-    config.order = "random"
+# Checks for pending migrations before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
-    config.before :each do
-      if Capybara.current_driver == :selenium
-        DatabaseCleaner.strategy = :truncation
-      else
-        DatabaseCleaner.strategy = :transaction
-      end
-      DatabaseCleaner.start
-    end
+RSpec.configure do |config|
+  # ## Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
 
-    config.after(:each) do
-      DatabaseCleaner.clean
-    end
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-    # Include Factory Girl syntax to simplify calls to factories
-    config.include FactoryGirl::Syntax::Methods
-  end
-end
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
 
-Spork.each_run do
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-  FactoryGirl.reload
-  include LoginMacros
+  # If true, the base class of anonymous controllers will be inferred
+  # automatically. This will be the default behavior in future versions of
+  # rspec-rails.
+  config.infer_base_class_for_anonymous_controllers = false
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = "random"
 end
